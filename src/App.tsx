b691bf4b91c5a4112a2df7093658d969617845ef
Ocean <dobinhduong1705@gmail.com>
2024-11-01 15:17:43 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import ThemeRoutes from "@/routes"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
 
